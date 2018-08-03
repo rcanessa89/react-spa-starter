d4d172df0fdc8224a93ccec0cdb92df1512cb58b
Canessa <rodolfo.canessa@gorillalogic.com>
2018-08-03 00:08:03 -0600

    scss added

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { AppRouter } from '@router';
 import store from '@store';
 import * as React from 'react';
 import { Provider } from 'react-redux';
-import './App.css';
 
 class App extends React.Component {
   public render() {
